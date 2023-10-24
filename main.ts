f2c619ec791c962fa971535c8f9baa38de958404
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-10-24 13:05:02 -0400

    typescript code

--- main.ts
+++ main.ts
@@ -1,8 +1,54 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Julianne Leblanc-Peltier
+ * Created on: Oct 2023
+ * This program generates two random integers between 0 - 99 and displays whether one is greater than the other.
 */
 
-basic.showString('Hello, World!')
+let firstRandomNumber: number = 0
+let secondRandomNumber: number = 0
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// if button a is pressed, displays randomly generated first number between 0 - 99.
+input.onButtonPressed(Button.A, function () {
+  // process
+  firstRandomNumber = randint(0, 99)
+  basic.clearScreen()
+  
+  // output
+  basic.showString('#1:')
+  basic.showNumber(firstRandomNumber)
+  basic.showIcon(IconNames.Happy)
+})
+
+// if button b is pressed, displays randomly generated second number between 0 - 99.
+input.onButtonPressed(Button.B, function () {
+  // process
+  secondRandomNumber = randint(0, 99)
+  basic.clearScreen()
+
+  // output
+  basic.showString('#2:')
+  basic.showNumber(secondRandomNumber)
+  basic.showIcon(IconNames.Happy)
+})
+
+// if MicroBit shaken, displays whether the first number is greater or lesser than second number
+input.onGesture(Gesture.Shake, function () {
+  basic.clearScreen()
+  // displays first number is greater than second number
+  if (firstRandomNumber > secondRandomNumber) {
+    basic.showNumber(firstRandomNumber)
+    basic.showString('>')
+    basic.showNumber(secondRandomNumber)
+    basic.showIcon(IconNames.Sad)
+  } else {
+    // displays first number is lesser than second number
+    basic.showNumber(firstRandomNumber)
+    basic.showString('<')
+    basic.showNumber(secondRandomNumber)
+    basic.showIcon(IconNames.Sad)
+  }
+})
